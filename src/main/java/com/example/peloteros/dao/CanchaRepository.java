9563cff20f22da4baedde6100808b99e3fc0d657
ricardorub <65383995+ricardorub@users.noreply.github.com>
2025-07-15 22:01:57 -0500

    Delete src directory

--- src/main/java/com/example/peloteros/dao/CanchaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.peloteros.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.peloteros.model.Cancha;
-
-public interface CanchaRepository extends JpaRepository<Cancha, Long> {
-}
